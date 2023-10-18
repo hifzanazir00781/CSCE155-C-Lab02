9cc7469b21a350a45697a015c11c21b3f9ada086
hifza <hifzanazir456@gmail.com>
2023-10-18 22:10:33 +0500

    prompt the user for amount to be calculated

--- dollar.c
+++ dollar.c
@@ -5,5 +5,9 @@ int main()
     float charges, remainingAmount, pounds;
     const float YEN_PER_USD=127.65;
     const float POUND_PER_USD=0.79f;
-    
+//prompt the user for the amount to be calculated
+ 
+ printf("please provide the amount to be converted");
+ scanf("%i",&totalAmount);
+ 
 }
