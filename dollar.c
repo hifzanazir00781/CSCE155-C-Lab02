b0004799d03a5a5f6499ee30a8d5363d1dbc7b7f
hifza <hifzanazir456@gmail.com>
2023-10-18 22:05:02 +0500

    make new sourse file and decleared the variables

---
+++ dollar.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int totalAmount,yens;
+    float charges, remainingAmount, pounds;
+    const float YEN_PER_USD=127.65;
+    const float POUND_PER_USD=0.79f;
+    
+}
